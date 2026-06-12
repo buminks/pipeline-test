abe435533d878af7969e74e7ab018b7a45238b7b
Bernhard Minks <bernhard@minks.de>
2026-06-12 13:53:56 +0200

    fix

--- src/de/firma/DockerUtils.groovy
+++ src/de/firma/DockerUtils.groovy
@@ -1,9 +1,18 @@
-def create(steps) {
-    return [
-        buildImage: { ->
-            steps.echo("Docker Image bauen")
-        }
-    ]
+class DockerUtils implements Serializable {
+
+    def steps
+
+    DockerUtils(steps) {
+        this.steps = steps
+    }
+
+    void buildImage() {
+        steps.echo("Docker Image bauen")
+    }
+}
+
+DockerUtils create(steps) {
+    return new DockerUtils(steps)
 }
 
 return this
--- src/de/firma/Utils.groovy
+++ src/de/firma/Utils.groovy
@@ -1,9 +1,18 @@
-def create(steps) {
-    return [
-        sayHello: { ->
-            steps.echo("Hallo")
-        }
-    ]
+class Utils implements Serializable {
+
+    def steps
+
+    Utils(steps) {
+        this.steps = steps
+    }
+
+    void sayHello() {
+        steps.echo("Hallo")
+    }
+}
+
+Utils create(steps) {
+    return new Utils(steps)
 }
 
 return this
